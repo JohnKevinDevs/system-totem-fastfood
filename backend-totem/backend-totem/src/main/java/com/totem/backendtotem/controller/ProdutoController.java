fee84e8d28c8e291d6addf77ac73c6010689232e
JohnKevinDevs <johnkevin.devs@gmail.com>
2026-03-30 17:55:39 -0300

    feat: update back-front end

--- backend-totem/backend-totem/src/main/java/com/totem/backendtotem/controller/ProdutoController.java
+++ backend-totem/backend-totem/src/main/java/com/totem/backendtotem/controller/ProdutoController.java
@@ -8,6 +8,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/produtos")
+@CrossOrigin(origins = "http://localhost:5173")
 public class ProdutoController {
 
     private final ProdutoRepository produtoRepository;
--- backend-totem/src/main/java/com/totem/backendtotem/repository/ProdutoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.totem.backendtotem.repository;
-
-import com.totem.backendtotem.model.Produto;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProdutoRepository extends JpaRepository<Produto, Long> {
-}
